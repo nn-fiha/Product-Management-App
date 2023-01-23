2fc946d41ba1237d257be06c982c63bfd74e7f94
nn-fiha <jucse28.361@gmail.com>
2023-01-23 12:31:29 +0600

    updaded added data

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 "use strict";
+let cnt=0;
 
 function getInput() {
   const id = document.getElementById("input1").value;
   const name = document.getElementById("input2").value;
   const price = document.getElementById("input3").value;
-
   return { id, name, price };
 }
 
 function createElement(text) {
   const row = document.createElement("tr");
-  const data = `<td>${text.id}</td><td>${text.name}</td><td>${text.price}</td>`;
-  row.innerHTML = data;
+  row.innerHTML = `<td>${text.id}</td><td>${text.name}</td><td>${text.price}</td>`;
   return row;
 }
-function createHeading() {
-    let row1 = document.createElement("tr");
-    let html = `<td>ID</td><td>Name</td><td>Price</td>`;
-    row1.innerHTML = html;
-    return row1;
+
+function addHeading(tblHead) {
+    
+    tblHead.innerHTML = `<th>ID</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Options</th>`;
+
   }
 
 function addData() {
   const inputData = getInput();
-  if (inputData.id === "" ){
-    alert(`Please fill up the "ID" field!!!`);
+  if (inputData.id === "" || inputData.name === "" || inputData.price === "" ){
+    alert("Please fill up all fields!!!");
     return;
   }
-  else if (inputData.name === "" ){
-    alert(`Please fill up the "Name" field !!!`);
-    return;
-  }
-  else if (inputData.price === "" ){
-    alert(`Please fill up the "price" field!!!`);
-    return;
-  }
- 
+  const tblHead = document.getElementById("t__head");
+  addHeading(tblHead);
   const row = createElement(inputData);
   const targetElement = document.getElementById("output__list");
   targetElement.appendChild(row);
+
 }
 addData();
